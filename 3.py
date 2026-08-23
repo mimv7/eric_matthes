863e09cc41c1373bbba32101ef523c823501baf1
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-08-23 19:07:42 +0300

    add 3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+#3.1-2
+names = ['andrey','slava','sasha']
+print(f'Hello, {names[0].title()}!')
+print(f'Hello, {names[1].title()}!')
+print(f'Hello, {names[2].title()}!')
+#3.3
+guitars = ['cort','martin','jet','fender']
+print(f'I want to buy "{guitars[0].title()}" guitars')
+print(f'I want to buy "{guitars[1].title()}" guitars')
+print(f'I want to buy "{guitars[2].title()}" guitars')
+print(f'I want to buy "{guitars[3].title()}" guitars')
