7590ea0b0e79ab53274327dc729abae1f7455826
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-08-24 19:11:46 +0300

    apgrade 3.py

--- 3.py
+++ 3.py
@@ -9,3 +9,33 @@ print(f'I want to buy "{guitars[0].title()}" guitars')
 print(f'I want to buy "{guitars[1].title()}" guitars')
 print(f'I want to buy "{guitars[2].title()}" guitars')
 print(f'I want to buy "{guitars[3].title()}" guitars')
+# 3.4
+guest = ['mama','papa','andrey']
+print(f'I invite you, {guest[0].title()}, to lunch!')
+print(f'I invite you, {guest[1].title()}, to lunch!')
+print(f'I invite you, {guest[2].title()}, to lunch!')
+# 3.5
+guest_cannot_come = guest.pop(2)
+print(f'The guest {guest_cannot_come.title()} cannot come')
+guest.append('sveta')
+print(f'\nI invite you, {guest[2].title()}, to lunch!')
+# 3.6
+guest.insert(0,'tanya')
+guest.insert(2,'kamila')
+guest.append('ivan')
+print(f'\n \tI invite you, {guest[0].title()}, to lunch!')
+print(f'\tI invite you, {guest[1].title()}, to lunch!')
+print(f'\tI invite you, {guest[2].title()}, to lunch!')
+print(f'\tI invite you, {guest[3].title()}, to lunch!')
+print(f'\tI invite you, {guest[4].title()}, to lunch!')
+print(f'\tI invite you, {guest[5].title()}, to lunch!')
+# 3.7
+guest_cannot_come = guest.pop(0)
+print(f'{guest_cannot_come.title()}- you cannot come')
+guest_cannot_come = guest.pop(1)
+print(f'{guest_cannot_come.title()}- you cannot come')
+del guest[-1]
+del guest[-1]
+
+for i in guest:
+    print(f'\n\tI invite you, {i.title()}, to lunch!')
