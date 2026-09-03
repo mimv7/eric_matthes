09d97e1a11c58af82dff0a2ed622d97b3d2ba23c
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-03 18:52:07 +0300

    apgrade 4.py

--- 4.py
+++ 4.py
@@ -33,3 +33,31 @@ print(cubes)
 # 4.9
 cubes =[line ** 3 for line in range(1,11)]
 print(cubes)
+
+# 4.10
+my_food = ['hamburger', 'glasses', 'chicken','beacon', 'milk' ]
+print(my_food[:3])
+print(my_food[1:3])
+print(my_food[-3:])
+
+# 4.11
+my_pizzas = ['margarita', '"4" cheese', 'dodo']
+wife_favorite_pizzas = my_pizzas[:]
+wife_favorite_pizzas.append('tashir')
+print('My favorite pizzas :')
+for my_pizza in my_pizzas:
+    print(f'\t{my_pizza}')
+print('')
+print('Wife favorite pizzas:')
+for wife_pizza in wife_favorite_pizzas:
+    print(f'\t{wife_pizza}')
+
+# 4.13
+buffet_in_adv = ('egg', 'sausage', 'cheese', 'yogurt', 'bread')
+print('buffet in advert:')
+for adv_menu in buffet_in_adv:
+    print(adv_menu)
+buffet_in_real = ('bread','water')
+print('\nmenu in real life:')
+for real_menu in buffet_in_real:
+    print(real_menu, end='!\n')
