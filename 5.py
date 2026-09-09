a2ea6cc4ae5f565ee4f3dc253c5fe7d69d6d7681
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-09 16:56:41 +0300

    add 5.py

--- 5.py
+++ 5.py
@@ -22,7 +22,8 @@ else:
 print(player_1_points)
 
 # 5.6
-age = int(input('input age - '))
+#age = int(input('input age - '))
+age = 12
 if age < 2:
     print('baby')
 elif 2 <= age < 4:
@@ -37,3 +38,44 @@ elif 65 <= age:
     print('elderly')
 else:
     print('wrong age')
+
+# 5.7
+fruits = ['qiwi','jerry', 'strawberry', 'melon', ' watermelon']
+favorite_fruits = 'jerry'
+if favorite_fruits in fruits:
+    print('True')
+else:
+    print('False')
+
+# 5.8
+user_login = ['arnold', 'meleshinrv','shi7ik','admin','login']
+for login in user_login:
+    if login.lower() == 'admin':
+        print('HELLO GOD OF COMPUTER')
+
+    else:
+        new_login = input('new login - ')
+        print(f'Hello, {new_login}!')
+        user_login.append(new_login)
+        break
+print(user_login)
+
+# 5.9
+user_login = []
+if user_login:
+    print(user_login)
+else:
+    new_login_user = input('input you login - ')
+    user_login.append(new_login_user)
+
+print(user_login)
+
+# 5.10
+current_users = ['arnold', 'meleshinrv', 'shi7ik', 'admin', 'login']
+new_users = ['Arnold', 'Dmitry', 'SHI7IK', 'olga', 'guest']
+current_users_lower = [user.lower() for user in current_users]
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"Имя '{new_user}' уже занято. Пожалуйста, выберите другое имя.")
+    else:
+        print(f"Имя '{new_user}' свободно. Добро пожаловать!")
