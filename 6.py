4dbef0cd1527e5f9094d5ad6de5837b0c8b50aad
Roman Meleshin <55110678+mimv7@users.noreply.github.com>
2026-09-15 19:30:37 +0300

    update 6.py

--- 6.py
+++ 6.py
@@ -27,3 +27,36 @@ glossary ={
 }
 for i in glossary:
     print(f'{i.title()} : \n\t{glossary[i]}')
+
+# 6.4
+for k,v in glossary.items():
+    print(f'{k} : {v}')
+
+# 6.5
+rivers = {
+    'nile':'egypt',
+    'neva':'russia',
+    'mississippi':'usa',
+    'oka':'russia'
+}
+for k,v in rivers.items():
+    print(f'the {k.title()} runs through {v.title()}')
+for rive in rivers.keys():
+    print(rive)
+
+for country in set(rivers.values()):
+    print(f'\t{country}')
+
+# 6.6
+favorite_lang ={
+    'jen':'python',
+    'sarah': 'c',
+    'edward':'ruby',
+    'phil':'python',
+}
+poll_participants = ['jen', 'alex', 'phil', 'elena', 'sarah', 'dylan']
+for name in poll_participants:
+    if name in favorite_lang.keys():
+        print(f'{name.title()}, спасибо за участие в опросе!"')
+    else:
+        print(f'{name.title()}, примите участие в опросе')
